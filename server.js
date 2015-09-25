6f2b92964f65ad3812a530ec7361d9e6dc43c9e0
Tali Walt <taliwalt@gmail.com>
2015-09-25 13:22:59 -0700

    Added a router for our api and cleaned up comments

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-//BASE SET UP
-
+//	BASE SET UP
+//	===========
 var express = require( 'express' ), //requires express 
 	app = express(), //defines the app is using express
 	bodyParser = require( 'body-parser' ), // grabs the body of a req
 	morgan = require( 'morgan' ), // for help with logging issues
-	mongoose = require ( 'mongoose' ); //requires mongoose
+	mongoose = require( 'mongoose' ); //requires mongoose
 
 
-
-//APP CONFIG
-
+//	APP CONFIG
+//	==========
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(morgan( 'dev' ))
 
-//connecting the DB
-
+//	DB
+//	==
 mongoose.connect('localhost:27017/GAems')
 
-//tells the app to use apiRouter
+//	ROUTES
+//	======
+var apiRouter = express.Router(); //create a router for our api
 app.use( 'api', apiRouter)
 
-//server 
+
+//	SERVER
+//	======
 app.listen(8080)
 console.log("The server is running on 8080")
